9e4db2c7460148b7e616caa8d79ec851356f8b9e
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2020-03-30 22:15:59 -0300

    adding dummy data

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,5 +1,9 @@
 const initState = {
-
+    projects: [
+        { id: '1', title: 'help me find peach', content: ' blah blah blah' },
+        { id: '2', title: 'collect all the stars', content: ' blah blah blah' },
+        { id: '3', title: 'egg hung with yoshi', content: ' blah blah blah' },
+    ]
 }
 
 const projecReducer = (state = initState, action) => {
