0fa119a55f54b9dfade67314d152a2ae85fd2928
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2020-04-11 11:26:52 -0300

    Cloud funtions firestore

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
-const Notifications = () => {
+const Notifications = (props) => {
     return (
-        <div>
-            <p>Notifications</p>
+        <div className="section">
+            <div className="card z-depth-0">
+                <div className="card-content">
+                    <span className="card-title">Notifications</span>
+                    <ul className="notifications">
+                        <ul>Notification</ul>
+                        <ul>Notification</ul>
+                        <ul>Notification</ul>
+                        <ul>Notification</ul>
+                        <ul>Notification</ul>
+                        <ul>Notification</ul>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
