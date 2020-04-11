a9a1c9858b646dbe13f3cdac509d9761801ace37
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2020-04-11 13:01:31 -0300

    deploy

---
+++ dist/precache-manifest.8516927cdc0f4d33912f82577b8cf5ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e54b12a07cbe95b21e7bb659e99fd6a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b9b664c272ab5d6f175",
+    "url": "/static/css/main.08b5ff87.chunk.css"
+  },
+  {
+    "revision": "e1cf61bfd814e5cdcd1c",
+    "url": "/static/js/2.1779e8e7.chunk.js"
+  },
+  {
+    "revision": "a7859b41c23bed46d2a2cd4146c86afb",
+    "url": "/static/js/2.1779e8e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b9b664c272ab5d6f175",
+    "url": "/static/js/main.a74d33c2.chunk.js"
+  },
+  {
+    "revision": "5ae5cdff232dc0214069",
+    "url": "/static/js/runtime-main.a8803374.js"
+  }
+]);
